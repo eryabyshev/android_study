931d10f8b65be6d89ba526750a5cf68b61c598e9
Евгений Рябышев <evgeny1709@gmail.com>
2019-12-28 14:13:01 +0300

    count down timer

--- Cool_Timer/app/src/main/java/ru/evgeny/cool_timer/MainActivity.java
+++ Cool_Timer/app/src/main/java/ru/evgeny/cool_timer/MainActivity.java
@@ -3,8 +3,8 @@ package ru.evgeny.cool_timer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
-import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,23 +14,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView viewById = findViewById(R.id.counter);
+        final TextView viewById1 = findViewById(R.id.counter1);
+        final TextView viewById2 = findViewById(R.id.counter2);
+        Handler handler = new Handler();
+        handler.post(simpleTimer(viewById1, handler));
 
-        final Handler handler = new Handler();
+        getCountDownTimer(viewById2).start();
+    }
 
+    private Runnable simpleTimer(final TextView text, final Handler handler) {
         Runnable runnable = new Runnable() {
-
             int counter = 0;
             @Override
             public void run() {
-
-                viewById.setText(counter + "");
+                text.setText(counter + "");
                 handler.postDelayed(this, 1000);
                 counter++;
-
             }
         };
 
-        handler.post(runnable);
+        return runnable;
+    }
+
+    private CountDownTimer getCountDownTimer(final TextView text) {
+        CountDownTimer timer = new CountDownTimer(10000, 1000) {
+            int counter = 10;
+
+            @Override
+            public void onTick(long l) {
+                text.setText(counter + " seconds left");
+                counter--;
+            }
+
+            @Override
+            public void onFinish() {
+                text.setText(counter + " seconds left - Finish");
+            }
+        };
+
+        return timer;
+
     }
 }
